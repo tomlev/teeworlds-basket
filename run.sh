5d36fadb9ab1e064cc328d7a982567b9d8181946
Thomas LEVIANDIER <thomas.leviandier@gmail.com>
2017-04-14 14:21:28 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-echo "sv_rcon_password $(pwgen -s 12 1)" >> /${GAME_TYPE}.cfg
-cat /${GAME_TYPE}.cfg
-exec /usr/games/teeworlds-server -f /${GAME_TYPE}.cfg
+exec /teeworlds_basket_srv -f /ctf.cfg
